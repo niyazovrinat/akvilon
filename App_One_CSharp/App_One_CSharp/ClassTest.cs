4be27619c9a65dda0cd18da879712b64b153800c
niyazovrinat <82271319+niyazovrinat@users.noreply.github.com>
2021-12-26 22:16:21 +0600

    - First Saving

---
+++ App_One_CSharp/App_One_CSharp/ClassTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_One_CSharp
+{
+    public class ClassTest
+    {
+        public static string GetTest() => "Test";
+    }
+}
---
+++ App_One_CSharp/App_One_CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_One_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# One: " + ClassTest.GetTest());
+            Console.Read();
+        }
+    }
+}
